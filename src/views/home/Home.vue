a8a7af1b207b61459efa3c0aac9680901f9f90e4
szq66 <1758417140@qq.com>
2020-08-05 11:10:19 +0800

    '代码同步更新'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,16 +4,61 @@
 		<home-swiper :banners="banners" />
 		<recommend-view :recommends="recommends" />
 		<feature-view />
+		<tab-control :titles="['流行', '新款', '精选']" class="tab-control" />
+		<ul>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+			<li>1</li>
+		</ul>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
 import FeatureView from './childComps/FeatureView'
 
-import {getHomeMultidata} from 'network/home'
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
+import {getHomeMultidata, getHomeGoods} from 'network/home'
 
 export default {
   name: 'Home',
@@ -21,19 +66,40 @@ export default {
 		NavBar,
 		HomeSwiper,
 		RecommendView,
-		FeatureView
+		FeatureView,
+		TabControl
   },
   data() {
 		return {
 			banners: [],
-			recommends: []
+			recommends: [],
+			goods: {
+				'pop': {page: 0, list: []},
+				'new': {page: 0, list: []},
+				'sell': {page: 0, list: []}
+			}
 		}
   },
   created() {
-		getHomeMultidata().then(res => {
-			this.banners = res.data.banner.list;
-			this.recommends = res.data.recommend.list
-		})
+		this.getHomeMultidata(),
+		this.getHomeGoods('pop'),
+		this.getHomeGoods('new'),
+		this.getHomeGoods('sell')
+  },
+  methods: {
+		getHomeMultidata() {
+			getHomeMultidata().then(res => {
+				this.banners = res.data.banner.list;
+				this.recommends = res.data.recommend.list
+			})
+		},
+		getHomeGoods(type) {
+			const page = this.goods[type].page + 1
+			getHomeGoods(type, page).then(res => {
+				this.goods[type].list.push(...res.data.list)
+				this.goods[type].page += 1
+			})
+		}
   }
 }
 </script>
@@ -50,5 +116,10 @@ export default {
 		right: 0;
 		top: 0;
 		z-index: 9;
+	}
+
+	.tab-control {
+		position: sticky;
+		top: 44px;
 	}
 </style>
